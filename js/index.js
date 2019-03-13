a386c0431438ce6280a91fe86e7e84843d46ddfd
Lorenzo Covarrubias Jr <lorenzocovarrubiasjr@gmail.com>
2019-03-13 02:33:52 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -22,6 +22,6 @@ function showRepositories() {
 
 function showCommits() {
   const commits = JSON.parse(this.responseText);
-  const commitsList = `<ul> ${commits.map(commit => '<li><strong>' + commit.author.login + '</strong> - ') + commit.commit.message + '</li>').join('')}</ul>`;
+  const commitsList = `<ul> ${commits.map(commit => '<li><strong>' + commit.author.login + '</strong> - ' + commit.commit.message + '</li>').join('')}</ul>`;
   document.getElementById('commits').innerHTML = commitsList;
 }
